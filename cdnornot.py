6501d48fb67cba82d68e16e01865a32efd72c317
omprakash gnawali <gnawali@gmail.com>
2017-08-24 13:15:08 -0500

    starter code

---
+++ cdnornot.py
@@ -0,0 +1,11 @@
+#your code in this file
+
+print "Fast downloaded files"
+print "URL", "Size(bytes)", "Download time(ms)"
+#print information about the files that downloaded quickly (top 50%)
+
+print "----------------"
+
+print "Slow downloaded files"
+print "URL", "Size(bytes)", "Download time(ms)"
+#print information about the files that downloaded slowly (bottom 50%)
